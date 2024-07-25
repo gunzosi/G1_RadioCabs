36d89fc54ea3ec17afd7c50b6476cfe32445e60b
Dang Dai Duong <127579534+gunzosi@users.noreply.github.com>
2024-07-25 14:04:11 +0700

    UPDATE FILTER BY CONTROLLER

---
+++ RadioCabs_v2/FeedbackServices/Controllers/FilterFeedbackController.cs
@@ -0,0 +1,77 @@
+﻿using FeedbackServices.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using RedisClient;
+using StackExchange.Redis;
+
+namespace FeedbackServices.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FilterFeedbackController : ControllerBase
+{
+    private readonly FeedbackRepository _dbContext;
+    private readonly IConfiguration _configuration;
+    private readonly REDISCLIENT _redisclient;
+        
+        
+    public FilterFeedbackController(FeedbackRepository dbContext, IConfiguration configuration, REDISCLIENT redisclient)
+    {
+        _dbContext = dbContext;
+        _configuration = configuration;
+        _redisclient = redisclient;
+    }
+    
+    // Test AUTHORIZE ADMIN
+    [HttpGet("testAuthorize")]
+    [Authorize(Roles = "Admin")]
+    public IActionResult TestAuthorize()
+    {
+        try
+        {
+            return Ok(new
+            {
+                StatusCode = 200,
+                Message = "You are authorized"
+            });
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    // TOP 10 new Feedback
+    [HttpGet("GetTop10Feedback")]
+    public async Task<IActionResult> GetTop10Feedback()
+    {
+        try
+        {
+            var feedbacks = await _dbContext.GetAllAsync();
+            var top10Feedbacks = feedbacks.OrderByDescending(f => f.CreatedAt).Take(10).ToList();
+            return Ok(top10Feedbacks);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+        }
+    }
+    
+    // GET MOST FEEDBACK BY CUSTOMER EMAIL
+    // [HttpGet("GetMostFeedbackByEmail")]
+    // public async Task<IActionResult> GetMostFeedbackByEmail(string emailPart)
+    // {
+    //     try
+    //     {
+    //         var feedbacks = await _dbContext.GetByCustomerEmailAsync(emailPart);
+    //         var mostFeedback = feedbacks.GroupBy(f => f.Email).OrderByDescending(g => g.Count()).FirstOrDefault();
+    //         return Ok(mostFeedback);
+    //     }
+    //     catch (Exception e)
+    //     {
+    //         return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+    //     }
+    // }
+}
--- RadioCabs_v2/FeedbackServices/obj/Debug/net8.0/FeedbackServices.AssemblyInfo.cs
+++ RadioCabs_v2/FeedbackServices/obj/Debug/net8.0/FeedbackServices.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FeedbackServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abfd85871badb3416f21aae6bfcb509c14c16d0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b03bc2b12e832c248cb1dfd18b47e82c53c89f70")]
 [assembly: System.Reflection.AssemblyProductAttribute("FeedbackServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FeedbackServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
